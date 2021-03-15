cc42cec6eaa1f94e8799b124f64024724b4d9026
Totori <290Livermorium@gmail.com>
2021-03-15 17:45:39 +0900

    ckeditorのプラグインを追加

--- web/src/js/ckeditor.js
+++ web/src/js/ckeditor.js
@@ -1,25 +1,48 @@
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
-import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+
 import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';     // <--- ADDED
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import StrikeThrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import SubScript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import SuperScript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+
+import List from '@ckeditor/ckeditor5-list/src/list';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 
 import axios from 'axios';
 import { getApiUrl, getViewFileUrl } from './url';
@@ -108,39 +131,61 @@ ClassicEditor.builtinPlugins = [
     Essentials,
     UploadAdapter,
     Autoformat,
-    Bold,
-    Italic,
     BlockQuote,
     EasyImage,
     Heading,
+    Link,
+    Paragraph,
+    Alignment,
+    CodeBlock,
+    Clipboard,
+
+    // フォントそのもの群
+    FontFamily,
+    FontColor,
+    FontBackgroundColor,
+    FontSize,
+    // 文字装飾群
+    Bold,
+    Italic,
+    StrikeThrough,
+    SubScript,
+    SuperScript,
+
+    // List
+    List,
+    TodoList,
+    Indent,
+
+    // 画像
     Image,
     ImageCaption,
     ImageStyle,
     ImageToolbar,
     ImageUpload,
-    Link,
-    List,
-    Paragraph,
-    Alignment,
-    CodeBlock,
+    ImageInsert,
+    MediaEmbed,
+
+    // Table
+    Table,
+    TableToolbar,
+    TableProperties,
+    TableCellProperties,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
     toolbar: {
         items: [
-            'heading',
-            '|',
-            'alignment',
-            'bold',
-            'italic',
-            'link',
-            'bulletedList',
-            'numberedList',
-            'ImageUpload',
-            'blockQuote',
-            'undo',
-            'redo',
+            'heading', '|',
+            'fontfamily', 'fontsize', 'fontcolor', 'fontbackgroundcolor', '|',
+            'bold', 'italic', 'strikethrough', 'subscript', 'superscript', '|',
+            'alignment', '|',
+            'bulletedList', 'numberedList', 'todoList', 'Outdent', 'Indent', '|',
+            'link', 'ImageInsert', 'MediaEmbed', '|',
+            'insertTable', '|',
+            'blockQuote', 'codeblock', '|',
+            'undo', 'redo',
         ],
     },
     image: {
@@ -150,6 +195,22 @@ ClassicEditor.defaultConfig = {
             '|',
             'imageTextAlternative',
         ],
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn', 'tableRow', 'mergeTableCells',
+            'tableProperties', 'tableCellProperties'
+        ],
+
+        // Configuration of the TableProperties plugin.
+        tableProperties: {
+            // ...
+        },
+
+        // Configuration of the TableCellProperties plugin.
+        tableCellProperties: {
+            // ...
+        }
     },
     // This value must be kept in sync with the language defined in webpack.config.js.
     language: 'jp',
