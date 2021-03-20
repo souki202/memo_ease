3e7118a650647dc26f017e341d17c43706eef953
Totori <290Livermorium@gmail.com>
2021-03-20 18:51:03 +0900

    ミス防止のため, デプロイ後にstagingに戻すよう変更

--- deploy_production.bat
+++ deploy_production.bat
@@ -7,3 +7,4 @@ call sam deploy --config-env production
 cd ..
 aws s3 sync ./web/dst s3://memo-ease.com --delete
 aws cloudfront create-invalidation --distribution-id E22XNYWXBJT4QW --paths "/*"
+git checkout staging
