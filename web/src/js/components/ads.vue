2c01db2faca659c9bed98247deee98dd7f8115c9
Totori <290Livermorium@gmail.com>
2021-04-17 11:36:57 +0900

    広告テスト

---
+++ web/src/js/components/ads.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6131667937099619"
+        data-ad-slot="2085728742"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    mounted() {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    },
+}
+</script>
